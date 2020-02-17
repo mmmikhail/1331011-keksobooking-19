b7efc235cb0fe4e6b9ef0a47c6f00c5b40885fd1
Mikhail Darkhovskii <mikednet@gmail.com>
2020-02-17 14:12:25 +0300

    Script corrected

--- js/main.js
+++ js/main.js
@@ -13,10 +13,9 @@ var roomFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'cond
 
 var nearectClassifiedArray = function () {
   var array = [];
-  for (var i = 0; i <= 1; i++) {
-    // var arrayElement = new Object();
-    // arrayElement.author = new Object();
+  for (var i = 0; i <= 7; i++) {
     var flat = roomType[getRandomInt(0, 3)];
+    var avatarNum = i + 1;
     var checkTimeIn = times[getRandomInt(0, 2)];
     var checkTimeOut = times[getRandomInt(0, 2)];
     var featuresLen = getRandomInt(1, 3);
@@ -25,7 +24,7 @@ var nearectClassifiedArray = function () {
       var featureNum = getRandomInt(0, 5);
       newRoomFeatures.push(roomFeatures[featureNum]);
     }
-    array.push({'author': {'avatar': 'img/avatars/user0' + i + 1 + '.png'}, 'offer': {'title': '', 'address': '600, 350', 'price': 100, 'type': flat, 'rooms': getRandomInt(1, 4), 'guests': getRandomInt(1, 10), 'checkin': checkTimeIn, 'checkout': checkTimeOut, 'features': newRoomFeatures, 'description': '', 'photos': ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg']}, 'location': {'x': 111, 'y': getRandomInt(130, 630)}});
+    array.push({'author': {'avatar': 'img/avatars/user0' + avatarNum + '.png'}, 'offer': {'title': '', 'address': '600, 350', 'price': 100, 'type': flat, 'rooms': getRandomInt(1, 4), 'guests': getRandomInt(1, 10), 'checkin': checkTimeIn, 'checkout': checkTimeOut, 'features': newRoomFeatures, 'description': '', 'photos': ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg']}, 'location': {'x': getRandomInt(10, 840), 'y': getRandomInt(130, 630)}});
   }
   return array;
 };
@@ -33,11 +32,12 @@ var nearectClassifiedArray = function () {
 var makeElement = function (obj) {
   var mapLabel = document.querySelector('#pin');
   var pin = mapLabel.content.cloneNode(true);
-  pin.classList.add('map__pin');
-  pin.setAttribute('style', 'left: ' + obj.location.x - 40 + 'px; top: ' + obj.location.y - 165 + 'px;');
+  var locationX = obj.location.x - 40;
+  var locationY = obj.location.y - 165;
+  pin.querySelector('.map__pin').setAttribute('style', 'left: ' +  locationX + 'px; top: ' + locationY + 'px;');
   var image = pin.querySelector('img');
-  image.src = obj.avatar;
-  image.alt = obj.title;
+  image.src = obj.author.avatar;
+  image.alt = obj.offer.title;
   return pin;
 };
 
@@ -48,7 +48,7 @@ map.classList.remove('map--faded');
 var arrayPins = nearectClassifiedArray();
 var collection = document.createDocumentFragment();
 
-for (var j = 0; j <= 2; j++) {
+for (var j = 0; j <= 7; j++) {
   var pinElement = makeElement(arrayPins[j]);
   collection.appendChild(pinElement);
 }
