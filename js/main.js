45357418396e636cfa6ee5f17094988904715a9d
Mikhail Darkhovskii <mikednet@gmail.com>
2020-02-20 13:00:13 +0300

    Script corrected

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var PIN_SIZE_X = 40;
+var PIN_SIZE_Y = 165;
+var N_PINS = 7;
+
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -11,9 +15,9 @@ var times = ['12:00', '13:00', '14:00'];
 var roomFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 //  var photos = [];
 
-var nearectClassifiedArray = function () {
+function nearectClassifiedArray() {
   var array = [];
-  for (var i = 0; i <= 7; i++) {
+  for (var i = 0; i <= N_PINS; i++) {
     var flat = roomType[getRandomInt(0, 3)];
     var avatarNum = i + 1;
     var checkTimeIn = times[getRandomInt(0, 2)];
@@ -24,22 +28,46 @@ var nearectClassifiedArray = function () {
       var featureNum = getRandomInt(0, 5);
       newRoomFeatures.push(roomFeatures[featureNum]);
     }
-    array.push({'author': {'avatar': 'img/avatars/user0' + avatarNum + '.png'}, 'offer': {'title': '', 'address': '600, 350', 'price': 100, 'type': flat, 'rooms': getRandomInt(1, 4), 'guests': getRandomInt(1, 10), 'checkin': checkTimeIn, 'checkout': checkTimeOut, 'features': newRoomFeatures, 'description': '', 'photos': ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg']}, 'location': {'x': getRandomInt(10, 840), 'y': getRandomInt(130, 630)}});
+    array.push({
+      'author': {
+        'avatar': 'img/avatars/user0' + avatarNum + '.png'
+      },
+      'offer': {
+        'title': '',
+        'address': '600, 350',
+        'price': getRandomInt(100, 10000),
+        'type': flat,
+        'rooms': getRandomInt(1, 4),
+        'guests': getRandomInt(1, 10),
+        'checkin': checkTimeIn,
+        'checkout': checkTimeOut,
+        'features': newRoomFeatures,
+        'description': '',
+        'photos': [
+          'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+          'http://o0.github.io/assets/images/tokyo/hotel2.jpg'
+        ]
+      },
+      'location': {
+        'x': getRandomInt(10, 840),
+        'y': getRandomInt(130, 630)
+      }
+    });
   }
   return array;
-};
+}
 
-var makeElement = function (obj) {
+function makeElement(obj) {
   var mapLabel = document.querySelector('#pin');
   var pin = mapLabel.content.cloneNode(true);
-  var locationX = obj.location.x - 40;
-  var locationY = obj.location.y - 165;
+  var locationX = obj.location.x - PIN_SIZE_X;
+  var locationY = obj.location.y - PIN_SIZE_Y;
   pin.querySelector('.map__pin').setAttribute('style', 'left: ' + locationX + 'px; top: ' + locationY + 'px;');
   var image = pin.querySelector('img');
   image.src = obj.author.avatar;
   image.alt = obj.offer.title;
   return pin;
-};
+}
 
 var map = document.querySelector('.map');
 var mapPins = document.querySelector('.map__pins');
@@ -48,7 +76,7 @@ map.classList.remove('map--faded');
 var arrayPins = nearectClassifiedArray();
 var collection = document.createDocumentFragment();
 
-for (var j = 0; j <= 7; j++) {
+for (var j = 0; j <= N_PINS; j++) {
   var pinElement = makeElement(arrayPins[j]);
   collection.appendChild(pinElement);
 }
