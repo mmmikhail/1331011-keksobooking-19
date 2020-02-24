786ff20b140c38a93c05d2e8efe8fb6f60d0d9d2
Mikhail Darkhovskii <mikednet@gmail.com>
2020-02-24 12:22:41 +0300

    Add missing variable

--- js/main.js
+++ js/main.js
@@ -19,6 +19,12 @@ function getRandomElementFromArray(array) {
 }
 
 var roomType = ['palace', 'flat', 'house', 'bungalo'];
+var roomNames = {
+  palace: 'Дворец',
+  flat: 'Квартира',
+  house: 'Дом',
+  bungalo: 'Бунгало'
+};
 var times = ['12:00', '13:00', '14:00'];
 var roomFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 
